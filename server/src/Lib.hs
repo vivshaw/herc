fe7c90497d154e043085d817bb300b2c36752db8
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2020-12-16 23:17:04 -0500

    server: add GraphQL Playground

--- server/src/Lib.hs
+++ server/src/Lib.hs
@@ -5,10 +5,15 @@ module Lib
     ) where
 
 import Web.Scotty
-
-import Data.Monoid (mconcat)
+import Network.HTTP.Types.Status (status404)
 
 serve :: IO ()
-serve = scotty 3000 $
+serve = scotty 3000 $ do
     get "/api" $ do
         html "<h1>herc api is up</h1>"
+    get "/graphql-playground" $ do
+        setHeader "Content-Type" "text/html; charset=utf-8"
+        file "src/views/graphql-playground.html"
+    notFound $ do
+        status status404
+        text "Not found"
